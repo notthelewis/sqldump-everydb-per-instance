7756b2584ce4f9f90521917ff3aab1f1164b7065
Lewis <43108320+notthelewis@users.noreply.github.com>
2020-04-24 14:25:53 +0100

    Create init.py

---
+++ init.py
@@ -0,0 +1,46 @@
+import mysql.connector as mysql
+import os
+import subprocess
+
+
+"""
+	Get's the current working directory, then gets the sqldump.sh file.
+	Syntax is - 
+	mysqldump db_name table_name > table_name.sql 
+  To use this, change any of the databse configuration settings. 
+  Also uses the mysql-connector package for python3.8. pip install mysql-connector-python  
+
+"""
+dir = os.getcwd()
+dumpcmd = dir + "/sqldump.sh"
+
+
+db = mysql.connect(
+    host = "localhost",
+    user = "USERNAME",
+    passwd = "PASSWORD",
+    database = "DATABASE_NAME"
+)
+
+passwd = "DATABASE PASSWORD"
+
+
+cursor = db.cursor(buffered=True)
+
+#	Calls the sheell command to sql dump, returns the output
+def _sqldump(table_name):
+    sp = subprocess.Popen([dumpcmd, db.user, passwd, db.database, table_name], stdout=subprocess.PIPE)
+    out = sp.stdout.readline()
+    return out
+
+#	Shows all tables in given database, stores results in res
+cursor.execute("SHOW TABLES")
+res = cursor.fetchall()
+
+
+with open("tables.txt", "w") as f:
+	f.writelines("%s\n" % result for result in res)
+
+with open("tables.txt") as f:
+	for i in f:
+		_sqldump(i)
